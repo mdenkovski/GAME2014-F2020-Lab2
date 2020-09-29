1d49cd2d8be8e2fce16a23a008c3068f36750bb7
Michael Denkovski <michaeldenkovski@hotmail.com>
2020-09-29 15:28:33 -0400

    Added in UI Score and Lives label scaling

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -9,32 +10,50 @@ public class GameController : MonoBehaviour
     public TMP_Text LivesLabel;
     public TMP_Text ScoreLabel;
 
+    public CanvasScaler scaler;
+
+    private float livesLabelHalfWidth;
+    private float livesLabelHalfHeight;
+    private float scoreLabelHalfWidth;
+    private float scoreLabelHalfHeight;
+
+   public Vector2 scale;
+
+    private void Start()
+    {
+        Vector2 ScreenRes = new Vector2(Screen.safeArea.width, Screen.safeArea.height);
+        scale = ScreenRes / scaler.referenceResolution;
+
+        livesLabelHalfWidth = scale.x * LivesLabel.rectTransform.rect.width * 0.5f;
+        livesLabelHalfHeight = scale.y * LivesLabel.rectTransform.rect.height * 0.5f;
+        scoreLabelHalfWidth = scale.x * ScoreLabel.rectTransform.rect.width * 0.5f;
+        scoreLabelHalfHeight = scale.y * ScoreLabel.rectTransform.rect.height * 0.5f;
+
+        
+
+        Rect safeArea = Screen.safeArea;
+        Debug.Log("X: " + safeArea.width);
+        Debug.Log("Y: " + safeArea.height);
+        Debug.Log("width: " + Screen.width);
+        Debug.Log("height: " + Screen.height);
+      
+        Debug.Log("safe area left: " + Screen.safeArea.xMin);
+        Debug.Log("safe area right: " + Screen.safeArea.xMax);
+
+        Debug.Log("half width: " + livesLabelHalfWidth);
+        Debug.Log("half width: " + livesLabelHalfHeight);
+        Debug.Log("Scaler ref:  " + scaler.referenceResolution);
+        Debug.Log("Scaler multiplier:  " + scale);
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-       switch(Input.deviceOrientation)
-        {
-            case DeviceOrientation.Portrait:
-                //SceneLabel.text = "Portrait";
-                LivesLabel.rectTransform.anchoredPosition = new Vector2(248.0f, -200.0f);
-                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-353.0f, -200.0f);
-                break;
-            case DeviceOrientation.LandscapeLeft:
-                //SceneLabel.text = "LandscapeLeft";
-                LivesLabel.rectTransform.anchoredPosition = new Vector2(370.0f, -83.0f);
-                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-353.0f, -83.0f);
-                break;
-            case DeviceOrientation.LandscapeRight:
-                //SceneLabel.text = "LandscapeRight";
-                LivesLabel.rectTransform.anchoredPosition = new Vector2(248.0f, -83.0f);
-                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-500.0f, -83.0f);
-                break;
-            case DeviceOrientation.Unknown:
-                //SceneLabel.text = "Unknown";
-                break;
-            default:
-               // SceneLabel.text = "Some other orientation";
-                break;
-        }
+
+        LivesLabel.rectTransform.position = new Vector2(Screen.safeArea.xMin + livesLabelHalfWidth, Screen.safeArea.yMax - livesLabelHalfHeight);
+        ScoreLabel.rectTransform.position = new Vector2(Screen.safeArea.xMax - scoreLabelHalfWidth, Screen.safeArea.yMax - scoreLabelHalfHeight);
+
+       
     }
 }
